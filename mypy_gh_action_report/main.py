e94d2d7deac1c3d05efe3d48370a60ec4b042d6f
bc291 <bazio1994@gmail.com>
2022-11-02 20:55:59 +0100

    Add line

--- mypy_gh_action_report/main.py
+++ mypy_gh_action_report/main.py
@@ -3,6 +3,7 @@ import sys
 
 def output_workflow_command():
     print("::error file=app.js,line=1::Missing semicolon", file=sys.stderr)
+    print("::error file=app.js,line=1::Missing semicolon", file=sys.stderr)
 
 
 if __name__ == "__main__":
