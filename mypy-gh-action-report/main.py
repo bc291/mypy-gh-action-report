9745cff37e5bb2813d7e5efb94738802c67ad2c5
bc291 <bazio1994@gmail.com>
2022-11-02 20:18:51 +0100

    initial

---
+++ mypy-gh-action-report/main.py
@@ -0,0 +1,6 @@
+import sys
+
+from mypy_json_report import produce_errors_report
+
+if __name__ == "__main__":
+    print("hello")
---
+++ tests/test_main.py
@@ -0,0 +1,6 @@
+def test():
+    assert 1 == 1
+
+
+def test_two():
+    assert 1 == 3
