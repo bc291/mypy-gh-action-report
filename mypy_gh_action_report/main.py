1578ee109dbea54bc4e9d1b4743b7f87ec239dc1
bc291 <bazio1994@gmail.com>
2022-11-02 20:42:10 +0100

    Use only py-3.11

--- mypy_gh_action_report/main.py
+++ mypy_gh_action_report/main.py
@@ -2,5 +2,10 @@ import sys
 
 from mypy_json_report import produce_errors_report
 
+
+def output_workflow_command():
+    print("::error file=app.js,line=1::Missing semicolon")
+
+
 if __name__ == "__main__":
-    print("hello")
+    output_workflow_command()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,2 @@
 def test():
-    print("::error file=app.js,line=1::Missing semicolon")
     assert 1 == 1
