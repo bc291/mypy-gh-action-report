5ea17680df66c050dff15791442588d3bd7e7601
bc291 <bazio1994@gmail.com>
2022-11-02 20:59:19 +0100

    reformat msg

--- mypy_gh_action_report/main.py
+++ mypy_gh_action_report/main.py
@@ -2,8 +2,7 @@ import sys
 
 
 def output_workflow_command():
-    print("::error file=app.js,line=1::Missing semicolon", file=sys.stderr)
-    print("::error file=app.js,line=1::Missing semicolon", file=sys.stderr)
+    print("::error file=main.js,line=1::Something awful!", file=sys.stderr)
 
 
 if __name__ == "__main__":
