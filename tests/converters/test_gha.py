8022e62a9b0aabb111a1b8a35405fdf0b0f85ce7
Błażej Cyrzon <36710760+bc291@users.noreply.github.com>
2022-12-01 22:04:01 +0100

    Switch to `github-actions-toolkit` (#24)
    * Switch to `github-actions-toolkit`

--- tests/converters/test_gha.py
+++ tests/converters/test_gha.py
@@ -54,14 +54,14 @@ def test_output_workflow_command(capsys, data):
     assert len(stdout_msg) == 5
 
     assert stdout_msg == [
-        "::error title=Error code%3A `arg-type`,file=app/core/core.py,line=18,endLine=,col=,endColumn=::"
+        "::error title=Error code%3A `arg-type`,file=app/core/core.py,line=18::"
         "Argument 1 to 'test' has incompatible type 'str'; expected 'int'",
-        "::error title=Error code%3A `assignment`,file=app/models/model.py,line=53,endLine=,col=,endColumn=::"
+        "::error title=Error code%3A `assignment`,file=app/models/model.py,line=53::"
         "Incompatible types in assignment (expression has type 'str', variable has type 'int')",
-        "::error title=Error code%3A `assignment`,file=app/models/model.py,line=54,endLine=,col=,endColumn=::"
+        "::error title=Error code%3A `assignment`,file=app/models/model.py,line=54::"
         "Incompatible types in assignment (expression has type 'str', variable has type 'int')",
-        "::error title=Error code%3A `assignment`,file=app/services/service.py,line=57,endLine=,col=,endColumn=::"
+        "::error title=Error code%3A `assignment`,file=app/services/service.py,line=57::"
         "Incompatible default for argument 'get_data' (default has type 'int', argument has type 'str')",
-        "::warning title=,file=app/services/service2/file1.py,line=66,endLine=,col=,endColumn=::"
+        "::warning file=app/services/service2/file1.py,line=66::"
         "Library stubs not installed for 'redis' (or incompatible with Python 3.10)",
     ]
